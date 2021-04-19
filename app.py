9538c821292381a17335c669070e38630a702283
asrafulashiq <asrafulashiq@gmail.com>
2021-04-19 03:21:08 -0400

    updated

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request
 from flask_ngrok import run_with_ngrok
-
-app = Flask(__name__)
-run_with_ngrok(app)  #starts ngrok when the app is run
-
+import argparse
 from transformers import pipeline, Conversation
 
-conversational_pipeline = pipeline("conversational",
-                                   model="microsoft/DialoGPT-medium")
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", "-p", type=int, default=None)
+parser.add_argument("--ngrok", "-n", action="store_true")
+parser.add_argument("--gpt_capacity", "-c", type=str, default="large")
+
+params = parser.parse_args()
+
+app = Flask(__name__)
+# run_with_ngrok(app)  #starts ngrok when the app is run
+
+conversational_pipeline = pipeline(
+    "conversational", model=f"microsoft/DialoGPT-{params.gpt_capacity}")
 
 conv = None
 
@@ -19,16 +26,26 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
     global conv
+    global conversational_pipeline
+
+    userText = request.args.get('msg')
+
     if conv is None:
         conv = Conversation(userText)
     else:
         conv.add_user_input(userText)
+
+    if "bye" in userText.lower():
+        conv = None
+        return ("bye\n" + ("-" * 30 + "\n") * 3)
+
     cpipe = conversational_pipeline([conv], device=1)
     response = cpipe.generated_responses[-1]
     return str(response)
 
 
 if __name__ == "__main__":
-    app.run()
+    if params.ngrok:
+        run_with_ngrok(app)
+    app.run(port=params.port)
