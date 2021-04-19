ad31a8440c14a79e9ae5c72cfc528415180074b3
asrafulashiq <asrafulashiq@gmail.com>
2021-04-19 02:32:05 -0400

    update

--- app.py
+++ app.py
@@ -4,22 +4,35 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)   #starts ngrok when the app is run
+run_with_ngrok(app)  #starts ngrok when the app is run
 
-#created chatbot with name john 
-#SQLStorageAdapter which allows the chat bot to connect to SQL databases. By default, this adapter will create a SQLite database.
-english_bot = ChatBot("John", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(english_bot)#allows the chat bot to be trained using data from the ChatterBot dialog corpus.
-trainer.train("chatterbot.corpus.english")# trainning based on  english greetings and conversations corpora 
+from transformers import pipeline, Conversation
+
+conversational_pipeline = pipeline("conversational",
+                                   model="microsoft/DialoGPT-medium")
+
+user_str = None
+conv = None
+cpipe = None
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+
+    if conv is None:
+        conv = Conversation(userText)
+    else:
+        conv.add_user_input(userText)
+    cpipe = conversational_pipeline([conv], device=1)
+    response = cpipe.generated_responses[-1]
+    return str(response)
+
 
 if __name__ == "__main__":
     app.run()
